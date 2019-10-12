7da4254f1b925601d7930bc5d421dd4430fb9595
BradPotts <brad.potts@phcnet.co>
2019-10-12 06:01:25 +0000

    add controller, updated devise config, add routes and updated application controller.

---
+++ app/controllers/phcdevworks_accounts/admin/accounts_controller.rb
@@ -0,0 +1,15 @@
+require_dependency "phcdevworks_accounts/application_controller"
+
+module PhcdevworksAccounts
+  class Admin::AccountsController < ApplicationController
+
+    # Admin Main Dashboard
+    def index
+    end
+
+    # Admin User List
+    def users_index
+    end
+
+  end
+end
--- app/controllers/phcdevworks_accounts/application_controller.rb
+++ app/controllers/phcdevworks_accounts/application_controller.rb
@@ -1,5 +1,30 @@
 module PhcdevworksAccounts
   class ApplicationController < ActionController::Base
+
+    # Devise Filter
+    before_action :phcdevworks_accounts_permitted_parameters, if: :devise_controller?
+
+    # Security Filters
     protect_from_forgery with: :exception
+
+    # Load Requried Helper Files
+    helper PhcdevworksActiveMenus::Engine.helpers
+    helper PhcdevworksNotifications::Engine.helpers
+    helper PhcdevworksTitleseo::Engine.helpers
+
+    private
+
+    # Whitelist Additional Fields
+    def phcdevworks_accounts_permitted_parameters
+      added_attrs = [:username, :firstname, :lastname, :email, :terms_of_service, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
+
+    # Redirect User to Root After Login
+    def after_sign_in_path_for(resource)
+      main_app.root_path
+    end
+
   end
 end
