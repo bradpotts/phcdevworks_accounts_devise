dd39fbc49e8b63e8305f787be9f0b71e6254bd00
BradPotts <brad.potts@phcnet.co>
2020-09-27 06:51:45 +0000

    changes to sidemenu.

--- app/controllers/phcdevworks_accounts/application_controller.rb
+++ app/controllers/phcdevworks_accounts/application_controller.rb
@@ -1,25 +1,25 @@
 module PhcdevworksAccounts
-  class ApplicationController < ActionController::Base
+    class ApplicationController < ActionController::Base
+    
+        # Devise Filter
+        before_action :phcdevworks_accounts_permitted_parameters, if: :devise_controller?
 
-    # Devise Filter
-    before_action :phcdevworks_accounts_permitted_parameters, if: :devise_controller?
+        # Security Filters
+        protect_from_forgery with: :exception
 
-    # Security Filters
-    protect_from_forgery with: :exception
+        # Load Requried Helper Files
+        helper PhcdevworksActiveMenus::Engine.helpers
+        helper PhcdevworksNotifications::Engine.helpers
+        helper PhcdevworksTitleseo::Engine.helpers
 
-    # Load Requried Helper Files
-    helper PhcdevworksActiveMenus::Engine.helpers
-    helper PhcdevworksNotifications::Engine.helpers
-    helper PhcdevworksTitleseo::Engine.helpers
+        private
 
-    private
+        # Whitelist Additional Fields
+        def phcdevworks_accounts_permitted_parameters
+        added_attrs = [:username, :firstname, :lastname, :email, :terms_of_service, :password, :password_confirmation, :remember_me]
+        devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+        devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+        end
 
-    # Whitelist Additional Fields
-    def phcdevworks_accounts_permitted_parameters
-      added_attrs = [:username, :firstname, :lastname, :email, :terms_of_service, :password, :password_confirmation, :remember_me]
-      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     end
-
-  end
 end
