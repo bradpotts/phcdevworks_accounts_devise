4d4304f3e80a3d23da853a51cc3b61f1a23871d8
BradPotts <brad.potts@phcnet.co>
2020-09-28 10:37:37 +0000

    add custom variables to be more flexible with the other auth engine.

--- app/helpers/phcdevworks_accounts/application_helper.rb
+++ app/helpers/phcdevworks_accounts/application_helper.rb
@@ -1,4 +1,41 @@
 module PhcdevworksAccounts
-  module ApplicationHelper
-  end
+    module ApplicationHelper
+        
+        # Auth - User Info
+        def user_first_name
+            current_user.firstname
+        end
+
+        # Auth - Last Name
+        def user_last_name
+            current_user.lastname
+        end
+
+        def user_profile_name
+            current_user.username
+        end
+
+        def user_avatar
+            current_user.gravatar_url
+        end
+
+        # Auth - Paths
+        def user_link_edit
+            phcdevworks_accounts.edit_user_registration_path
+        end
+
+        def user_link_logout
+            phcdevworks_accounts.destroy_user_session_path
+        end
+
+        def admin_link_index
+            phcdevworks_accounts.admin_users_index_path
+        end
+
+
+
+
+
+
+    end
 end
