65690fc882fe6758aa3f8a7778e1fd7d1da9a9a3
BradPotts <brad.potts@phcnet.co>
2020-12-21 11:49:45 +0000

    standardized the manifest test file.

--- test/test_app/app/assets/config/manifest.js
+++ test/test_app/app/assets/config/manifest.js
@@ -1,7 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link phcthemes_admin_panel_pack_coloradmin.css
-//= link phcthemes_admin_panel_pack_coloradmin.js
-//= link phcdevworks_accounts_manifest.js
 //= link phcdevworks_accounts/application.css
 //= link phcdevworks_accounts/application.js
+//= link phcdevworks_core_modules/application.css
+//= link phcdevworks_core_modules/application.js
+//= link phcthemes_admin_panel_pack_coloradmin.css
+//= link phcthemes_admin_panel_pack_coloradmin.js
