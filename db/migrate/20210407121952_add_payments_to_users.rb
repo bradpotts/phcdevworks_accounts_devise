080df5f269e607242a5383ca67080bde694b5865
BradPotts <brad.potts@phcnet.co>
2021-04-08 13:57:08 +0000

    add customer id to users table.

--- db/migrate/20210407121952_add_payments_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddPaymentsToUsers < ActiveRecord::Migration[6.1]
-    def change
-
-        add_column :phcdevworks_accounts_users, :payments_customer_id, :string
-        add_column :phcdevworks_accounts_users, :payments_subscription_id, :string
-
-    end
-end
