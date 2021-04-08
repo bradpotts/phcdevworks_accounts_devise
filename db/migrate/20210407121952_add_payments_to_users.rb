257f359e845863936ac3d2f7b4cece47b50b613b
BradPotts <brad.potts@phcnet.co>
2021-04-08 13:54:55 +0000

    removed plan_id before gem release.

--- db/migrate/20210407121952_add_payments_to_users.rb
+++ db/migrate/20210407121952_add_payments_to_users.rb
@@ -3,7 +3,6 @@ class AddPaymentsToUsers < ActiveRecord::Migration[6.1]
 
         add_column :phcdevworks_accounts_users, :payments_customer_id, :string
         add_column :phcdevworks_accounts_users, :payments_subscription_id, :string
-        add_column :phcdevworks_accounts_users, :payments_plan_id, :string
 
     end
 end
